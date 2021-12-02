dc52a654dafda480cdb08b9b723c56c673f3beac
alvesluiz23 <alvesluizeduardo23@gmail.com>
2021-12-02 17:38:24 -0300

    Adicionando aula 2

---
+++ Variáveis_Tipos_de_Dados_e_Operadores_Matemáticos_em_Java/src/aula2/Main.java
@@ -0,0 +1,30 @@
+package aula2;
+
+public class Main {
+      public static void main(String[] args) {
+		    byte b1 = 10;
+		    byte b2 = 20;
+		    
+		    short s1 = 20000;
+		   // short s2 = 40000;
+		    
+		    long i1 = 100000000000L;
+		    long i2 = 200000000000L;
+		    
+		    //float f1 = 4.5;
+		    float f2 = 10.00f;
+		    
+		    double d1 = 65.69;
+		    double d2 = 70.20;
+		    
+		    char c1 = 'a';
+		    //char c2 = 'am';
+		    char c3 = '\u0057';
+		    
+		    String st1 = "Fulano";
+		    String st2 = "Cicrano";
+		    //String st3 = 'Seila o que ele escreveu';
+		    
+		    
+	}
+}
